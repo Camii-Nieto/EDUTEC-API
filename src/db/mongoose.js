e75ead5eae82c0179ee51999bae76c11a4a23562
Camii-Nieto <you@example.com>
2021-08-19 00:02:24 -0300

    grrrrrr

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://EDUTEC:<password>@cluster0.kena6.mongodb.net/edutec?retryWrites=true&w=majority";
+const uri = "mongodb+srv://EDUTEC:4944208@cluster0.kena6.mongodb.net/edutec?retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
